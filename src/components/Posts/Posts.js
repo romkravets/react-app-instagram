c2370cf7ab62718f0269ca63b5a2aae98cb62aaf
romkravets <romann.kravets@gmail.com>
2019-05-28 20:56:02 +0300

    Added service class, async aweit, json-server

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -1,13 +1,74 @@
 import React, {Component} from 'react';
+import User from '../user/User';
+import InstaService from '../../services/instaservice';
+import ErrorMessage from '../errorMessage/errorMessage';
 import './Posts';
-import Post from '../post/Post';
 
 export default class Posts extends Component {
+   InstaService = new InstaService();
+   state = {
+      posts: [],
+      error: false
+   }
+
+   componentDidMount() {
+      this.updatePosts();
+   }
+
+   updatePosts() {
+      this.InstaService.getAllPosts()
+      .then(this.onPostLoaded)
+      .catch(this.onError);
+   }
+
+   onPostLoaded = (posts) => {
+      this.setState({
+         posts,
+         error: false
+      })
+   }
+
+   onError = () => {
+      this.setState({
+         error: true
+      })
+   }
+
+   renderItems(arr) {
+      return arr.map((item) => {
+         const { name, altname, photo, src, alt, descr, id } = item;
+
+         return (
+            <div key={id} className="post">
+               <User
+                  src={photo}
+                  alt={altname}
+                  name={name}
+                  min
+               />
+            <img src={src} alt={alt}></img>
+            <div className="post__name">
+               {name}
+            </div>
+            <div className="post__descr">
+               {descr}
+            </div>
+         </div>
+         )
+      })
+   }
+
    render() {
+      const {error, posts} = this.state;
+      if (error) {
+         return <ErrorMessage/>
+      }
+
+      const items = this.renderItems(posts);
+
       return (
          <div className="left">
-            <Post alt="nature" src="https://images5.alphacoders.com/440/440292.jpg"/>
-            <Post alt="nature" src="https://images5.alphacoders.com/440/440292.jpg"/>
+            {items}
          </div>
       )
    }
---
+++ src/components/errorMessage/errorMessage.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import errorImg from '../../error.png';
+
+const ErrorMessage = () => {
+   return (
+      <>
+         <img src={errorImg} alt="error"></img>
+      </>
+   )
+}
+
+export default ErrorMessage;
